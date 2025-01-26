13184b33230dfaaada8e86731b2ab45aca842715
tymur_lykho <tima.likho@gmail.com>
2025-01-26 17:35:03 +0200

    Do homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,25 @@
-function getElementWidth(content, padding, border) { 
-	const cont = Number.parseFloat(content);
-	const allPaddingWidth = Number.parseFloat(padding) * 2;
-	const allBorderWidth = Number.parseFloat(border) * 2;
-	const result = cont + allPaddingWidth + allBorderWidth;
+const profile = {
+    username: "Jacob",
+	playTime: 300,
+	changeUsername(newName) { 
+		this.username = newName;
+	},
+	updatePlayTime(hours) {
+		this.playTime += hours;
+	},
+	getInfo() {
+		return `${this.username} has ${this.playTime} active hours!`
+	 },
 
-	return result;
-}
+};
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
 
